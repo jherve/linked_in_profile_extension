ce030db328cb0c708380be262c03654d40959ed1
jherve <julien.jev.herve@gmail.com>
2023-11-30 18:09:39 +0100

    Settings page is useless after all

--- settings/options.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", async (e) => {
-    const jobsPath = await browser.storage.sync.get("jobsPath");
-    document.querySelector("#jobs-path").value = jobsPath.jobsPath || "";
-});
-
-document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    browser.storage.sync.set({jobsPath: document.querySelector("#jobs-path").value});
-});
